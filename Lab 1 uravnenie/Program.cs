eb05d008ab341e1ee6b9ef19d1093b3bd9689e3c
vlad <mr.xeris@mail.ru>
2025-02-09 00:38:56 +0200

    uravnenie

--- Lab 1 uravnenie/Program.cs
+++ Lab 1 uravnenie/Program.cs
@@ -4,8 +4,8 @@ class Program
 {
     static void Main()
     {
-        double x = 1.0; // Задайте значение x
-        double y = 2.0; // Задайте значение y
+        double x = 1.0;
+        double y = 2.0; 
 
         double result = Math.Pow(2, -x) - Math.Cos(x) + Math.Sin(2 * x * y);
 
