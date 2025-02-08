416c65805c34ad9f52bc46ce8844198e330ce00b
vlad <mr.xeris@mail.ru>
2025-02-09 00:35:02 +0200

    Uravnenie

---
+++ Lab 1 uravnenie/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double x = 1.0; // Задайте значение x
+        double y = 2.0; // Задайте значение y
+
+        double result = Math.Pow(2, -x) - Math.Cos(x) + Math.Sin(2 * x * y);
+
+        Console.WriteLine($"Результат: {result}");
+    }
+}
